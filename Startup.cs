46e0f0109024fbcaa7e051b45f5376363d6582d8
Kay Sugiyama <keifuku.sugiyama@gmail.com>
2018-12-13 17:22:43 -0800

    delteed cookie policy

--- Startup.cs
+++ Startup.cs
@@ -26,12 +26,6 @@ namespace ECommerce
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
@@ -54,9 +48,7 @@ namespace ECommerce
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
             app.UseSession();
 
             app.UseMvc(routes =>
